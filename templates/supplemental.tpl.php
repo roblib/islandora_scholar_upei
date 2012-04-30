84e02806a7e91b34815cb924136184aecfff099c
rwincewicz <richard.wincewicz@googlemail.com>
2012-04-30 00:42:22 -0300

    Moved supplemental information to template file

---
+++ templates/supplemental.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div id="clear-block">
+  <b>Supplemental information</b></br>
+
+    <table>
+        <?php foreach ($supp_header as $header): ?>
+        <th>
+        <?php print $header ?>
+        </th>
+        <?php endforeach; ?>
+    <?php foreach ($supp_rows as $row): ?>
+      <tr class="odd">  
+        <?php foreach ($row as $r): ?>
+
+          <td><?php print $r ?></td>
+
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+  
+  <p>Name: <?php print l($zip_description, 'download_ds/' . $pid . '/ZIP') ?></br>
+    <?php print t('Size: @size bytes', array('@size' => $zip_size))?></br>
+
+
+</div>
