a6d8b7ffdfe8426cb8d6fd9bd5993816460a4493
rwincewicz <richard.wincewicz@googlemail.com>
2012-04-29 23:18:25 -0300

    Moved scholar tracking to template file

--- templates/scholar.tpl.php
+++ templates/scholar.tpl.php
@@ -24,6 +24,10 @@
 
   <?php print $citation ?>
   
+  </br><div>Times viewed: <?php print $times_read ?></div>
+  <div>Last viewed: <?php print $time_last_viewed?></div></br>
+
+  
   <div> <A href="http://www.refworks.com/express/ExpressImport.asp?vendor=Robertson+Library&filter=RIS%20Format&url=http%3A//<?php print $server_ip ?>/refworks_entry/<?php print urlencode($pid) ?>" target="RefWorksMain">Export to RefWorks</A></div>
 
   <div class="scholar_coins_title"><span>  <br /><span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.issn=<?php print $issn ?>&amp;rft.date=<?php print $date ?>&amp;rft.volume=<?php print $volume ?>&amp;rft.issue=<?php print $issue ?>&amp;rft.spage=<?php print $start_page ?>&amp;rft.atitle=<?php print $title ?>&amp;rft.jtitle=<?php print $journal_title ?>"></span>
