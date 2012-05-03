3d6b29c5f41dcffc6bd57fb8331817d9abb33b20
rwincewicz <richard.wincewicz@googlemail.com>
2012-05-03 10:19:59 -0300

    Moved tracking display to template file

---
+++ templates/tracking.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div id="clear-block">
+
+  File has been downloaded <?php print $tracking_count ?> times</br>
+  Last downloaded: <?php print $tracking_date ?>
+
+</div>
