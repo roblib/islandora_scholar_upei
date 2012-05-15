33eba612bc8ab36a10e96f053c07c20470403355
rwincewicz <richard.wincewicz@googlemail.com>
2012-05-15 13:09:18 -0300

    Moved elements around in citation view

--- templates/scholar.tpl.php
+++ templates/scholar.tpl.php
@@ -25,6 +25,6 @@
 
   <div class="scholar_coins_title"><span>  <br /><span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.issn=<?php print $issn ?>&amp;rft.date=<?php print $date ?>&amp;rft.volume=<?php print $volume ?>&amp;rft.issue=<?php print $issue ?>&amp;rft.spage=<?php print $start_page ?>&amp;rft.atitle=<?php print $title ?>&amp;rft.jtitle=<?php print $journal_title ?>"></span>
 
-  </div><div><br/><A HREF="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;issn=<?php print $issn ?>&date=<?php print $date ?>&volume=<?php print $volume ?>&issue=<?php print $issue ?>&spage=<?php print $start_page ?>&atitle=<?php print $title ?>&stitle=<?php print $journal_title ?>" target="_blank">UPEI Users Only: Check for Full Text</A></span><br /></div>
+  </div><div class="scholar_coins_url"><br/><A HREF="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;issn=<?php print $issn ?>&date=<?php print $date ?>&volume=<?php print $volume ?>&issue=<?php print $issue ?>&spage=<?php print $start_page ?>&atitle=<?php print $title ?>&stitle=<?php print $journal_title ?>" target="_blank">UPEI Users Only: Check for Full Text</A></span><br /></div>
 
 </div>
