dbb717210911d1c6699c46853c7371ece6b10b07
rwincewicz <richard.wincewicz@googlemail.com>
2012-05-15 12:05:01 -0300

    Moved elements around in citation view

--- templates/tracking.tpl.php
+++ templates/tracking.tpl.php
@@ -1,9 +1,3 @@
-<?php
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
 <div id="clear-block">
 
   File has been downloaded <?php print $tracking_count ?> times</br>
