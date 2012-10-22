48a003481e89007b18c241c5953126b88709354c
Paul Pound <ppound@upei.ca>
2012-10-22 10:06:04 -0300

    changes to only show embargoed pdfs after embargo date

--- templates/scholar.tpl.php
+++ templates/scholar.tpl.php
@@ -22,23 +22,35 @@
 
   <h3><?php print t('Adding this citation to "My List" will allow you to export this citation in other styles.'); ?></h3>
 
+  <div class="is-fullview-btns">
+    <?php if (isset($add_to_list)): ?>
+      <?php print $add_to_list; ?>
+    <?php endif; ?>
+    <?php if (isset($full_text) && !$embargoed): ?>
+      <?php print $full_text; ?>
+    <?php endif; ?>
+    <div class="islandscholar-refworks"><a href="http://rlproxy.upei.ca/login?url=http://www.refworks.com/express/ExpressImport.asp?vendor=Robertson+Library&filter=RIS%20Format&url=<?php print $base_url ?>/refworks_entry/<?php print urlencode($pid) ?>" target="RefWorksMain" class="button refworks">Export to RefWorks</A></div>
+    <?php if (stripos((string) $genre, 'book') !== FALSE && (!isset($full_text) || $embargoed )): ?>
+      <div class="scholar_coins_title">
+          <span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.isbn=<?php print $isbn ?>&amp;rft.date=<?php print $date ?>&amp;rft.title=<?php print $title ?>">
+          </span>
+      </div>
+      <div class="scholar_coins_url">
+        <a href="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;isbn=<?php print $isbn ?>&date=<?php print $date ?>&title=<?php print $title ?>" target="_blank">Check for Full Text</a>
+      </div>
+    <?php endif; ?>
+    <?php if (stripos((string) $genre, 'book') === FALSE && (!isset($full_text) || $embargoed )): ?>
+      <div class="scholar_coins_title">
+        <span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.issn=<?php print $issn ?>&amp;rft.date=<?php print $date ?>&amp;rft.volume=<?php print $volume ?>&amp;rft.issue=<?php print $issue ?>&amp;rft.spage=<?php print $start_page ?>&amp;rft.atitle=<?php print $title ?>&amp;rft.jtitle=<?php print $journal_title ?>">
+        </span>
+      </div>
+      <div class="scholar_coins_url">
+        <a href="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;issn=<?php print $issn ?>&date=<?php print $date ?>&volume=<?php print $volume ?>&issue=<?php print $issue ?>&spage=<?php print $start_page ?>&atitle=<?php print $title ?>&stitle=<?php print $journal_title ?>" target="_blank">Check for Full Text</a>
+      </div>
+    <?php endif; ?>
+    <?php if (isset($associate_button)): ?>
+      <?php print $associate_button; ?>
+    <?php endif; ?>
+  </div>
 
-  <div class="scholar_refworks_link"> <A href="http://rlproxy.upei.ca/login?url=http://www.refworks.com/express/ExpressImport.asp?vendor=Robertson+Library&filter=RIS%20Format&url=<?php print $base_url ?>/refworks_entry/<?php print urlencode($pid) ?>" target="RefWorksMain" class="button refworks">Export to RefWorks</A></div>
-  <?php if (stripos((string) $genre, 'book') !== FALSE && !isset($full_text)): ?>
-    <div class="scholar_coins_title"><span>  <br /><span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.isbn=<?php print $isbn ?>&amp;rft.date=<?php print $date ?>&amp;rft.title=<?php print $title ?>"></span>
-    </div><div class="scholar_coins_url"><br/><A HREF="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;isbn=<?php print $isbn ?>&date=<?php print $date ?>&title=<?php print $title ?>" target="_blank">Check for Full Text</A></span><br /></div>
-  <?php endif; ?>
-    <?php if (stripos((string) $genre, 'book') === FALSE && !isset($full_text)): ?>
-  <div class="scholar_coins_title"><span>  <br /><span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.issn=<?php print $issn ?>&amp;rft.date=<?php print $date ?>&amp;rft.volume=<?php print $volume ?>&amp;rft.issue=<?php print $issue ?>&amp;rft.spage=<?php print $start_page ?>&amp;rft.atitle=<?php print $title ?>&amp;rft.jtitle=<?php print $journal_title ?>"></span>
-  </div><div class="scholar_coins_url"><br/><A HREF="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;issn=<?php print $issn ?>&date=<?php print $date ?>&volume=<?php print $volume ?>&issue=<?php print $issue ?>&spage=<?php print $start_page ?>&atitle=<?php print $title ?>&stitle=<?php print $journal_title ?>" target="_blank">Check for Full Text</A></span><br /></div>
- <?php endif; ?>
-  <?php if (isset($full_text)): ?>
-    <?php print $full_text; ?>
-   <?php endif; ?>
-  <?php if (isset($add_to_list)): ?>
-    <?php print $add_to_list; ?>
-   <?php endif; ?>
-  <?php if (isset($associate_button)): ?>
-    <?php print $associate_button; ?>
-   <?php endif; ?>
 </div>
