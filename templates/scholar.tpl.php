525104675e3e89038cfa73c07d980a4ceec70955
Paul Pound <ppound@upei.ca>
2014-03-21 16:08:50 -0300

    urlencoded some variables for the coins url

--- templates/scholar.tpl.php
+++ templates/scholar.tpl.php
@@ -42,20 +42,20 @@
     <div class="scholar-refworks"><a href="http://rlproxy.upei.ca/login?url=http://www.refworks.com/express/ExpressImport.asp?vendor=Robertson+Library&filter=RIS%20Format&url=<?php print $base_url ?>/refworks_entry/<?php print urlencode($pid) ?>" target="RefWorksMain" class="button refworks">Export to RefWorks</A></div>
     <?php if (stripos((string) $genre, 'book') !== FALSE && (!isset($full_text) || $embargoed )): ?>
       <div class="scholar_coins_title">
-        <span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.isbn=<?php print $isbn ?>&amp;rft.date=<?php print $date ?>&amp;rft.title=<?php print $title ?>">
+        <span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.isbn=<?php print $isbn ?>&amp;rft.date=<?php print $date ?>&amp;rft.title=<?php print urlencode($title) ?>">
         </span>
       </div>
       <div class="scholar_coins_url">
-        <a href="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;isbn=<?php print $isbn ?>&date=<?php print $date ?>&title=<?php print $title ?>" target="_blank" class="check-for-text"><img src="http://library.upei.ca/sites/all/files/sharedfolder/files/getit_upei.gif"></a>
+        <a href="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;genre=<?php print urlencode($genre) ?>&isbn=<?php print $isbn ?>&date=<?php print urlencode($date) ?>&title=<?php print urlencode($title) ?>" target="_blank" class="check-for-text"><img src="http://library.upei.ca/sites/all/files/sharedfolder/files/getit_upei.gif"></a>
       </div>
     <?php endif; ?>
     <?php if (stripos((string) $genre, 'book') === FALSE && (!isset($full_text) || $embargoed )): ?>
       <div class="scholar_coins_title">
-        <span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.issn=<?php print $issn ?>&amp;rft.date=<?php print $date ?>&amp;rft.volume=<?php print $volume ?>&amp;rft.issue=<?php print $issue ?>&amp;rft.spage=<?php print $start_page ?>&amp;rft.atitle=<?php print $title ?>&amp;rft.jtitle=<?php print $journal_title ?>">
+        <span class="Z3988" title="&#10;&#9;&#9;&#9;&#9;ctx_ver=Z39.88-2004&amp;rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&amp;rfr_id=info:sid/library.upei.ca:Robertson&amp;rft.issn=<?php print $issn ?>&amp;rft.date=<?php print urlencode($date) ?>&amp;rft.volume=<?php print urlencode($volume) ?>&amp;rft.issue=<?php print urlencode($issue) ?>&amp;rft.spage=<?php print $start_page ?>&amp;rft.atitle=<?php print urlencode($title) ?>&amp;rft.jtitle=<?php print urlencode($journal_title) ?>">
         </span>
       </div>
       <div class="scholar_coins_url">
-        <a href="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;issn=<?php print $issn ?>&date=<?php print $date ?>&volume=<?php print $volume ?>&issue=<?php print $issue ?>&spage=<?php print $start_page ?>&atitle=<?php print $title ?>&stitle=<?php print $journal_title ?>" target="_blank" class="check-for-text"><img src="http://library.upei.ca/sites/all/files/sharedfolder/files/getit_upei.gif"></a>
+        <a href="&#10;&#9;&#9;&#9;&#9;http://articles.library.upei.ca:7888/godot/hold_tab.cgi?hold_tab_branch=PCU&amp;genre=<?php print urlencode($genre) ?>&issn=<?php print $issn ?>&date=<?php print urlencode($date) ?>&volume=<?php print urlencode($volume) ?>&issue=<?php print urlencode($issue) ?>&spage=<?php print urlencode($start_page) ?>&atitle=<?php print urlencode($title) ?>&stitle=<?php print urlencode($journal_title) ?>" target="_blank" class="check-for-text"><img src="http://library.upei.ca/sites/all/files/sharedfolder/files/getit_upei.gif"></a>
       </div>
     <?php endif; ?>
     <?php if (isset($associate_button)): ?>
